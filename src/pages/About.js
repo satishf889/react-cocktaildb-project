ee20a304fcf880d9623859a80635aa9f3f885490
Satish Fulwani <satish.fulwani63@gmail.com>
2020-05-16 18:19:15 +0530

    Add content to About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section class="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat id,
+        officia quidem ullam corrupti ad dolor natus ipsam perferendis. Deserunt
+        porro sequi, natus neque explicabo ipsum quod! Tempore, unde animi?
+      </p>
+    </section>
+  );
 }
