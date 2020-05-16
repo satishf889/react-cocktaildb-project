26b1209bd63b6e957d44c9d7925c44e21456dcb2
Satish Fulwani <satish.fulwani63@gmail.com>
2020-05-16 20:31:45 +0530

    Add Searchform and css

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,33 @@
 import React from "react";
 
-export default function SearchForm() {
-  return <h1>searchform component</h1>;
+export default function SearchForm({ setSearchTerm }) {
+  //useRef is used to store value of input
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchValue = React.useRef("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  const searchcocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  return (
+    <section className="section">
+      <h2 className="section-title">search cocktails</h2>
+      <form className="form search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={searchcocktail}
+            ref={searchValue}
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
