d48cb1b894f83a57b4246a76eb9d31a664a6543b
Satish Fulwani <satish.fulwani63@gmail.com>
2020-05-16 18:04:57 +0530

    Add Routes for all pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//import pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
 
 export default function App() {
-  return <h1>app component</h1>;
+  return (
+    <Router>
+      {/* Switch is used to show first exact match path */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
