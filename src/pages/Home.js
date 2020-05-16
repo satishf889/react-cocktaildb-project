8cde740df7e3a1d86f0737c1cb3cd0f7c94628a4
Satish Fulwani <satish.fulwani63@gmail.com>
2020-05-16 18:53:20 +0530

    Structure APi call and data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,61 @@
 import React from "react";
+import CocktailsList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
-  return <h1>Home page</h1>;
+  //Setting states in functional components
+
+  //Loading state
+  const [loading, setLoading] = React.useState(false);
+
+  //Search state
+  const [searchTerm, setSearchTerm] = React.useState("a");
+
+  //Cocktails state
+  const [cocktails, setCocktails] = React.useState(false);
+
+  //Using empty array[] as second array make to run useEffect ony ones when component mounts
+  React.useEffect(() => {
+    setLoading(true);
+    async function getDrinks() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        );
+        const data = await response.json();
+        const { drinks } = data;
+        if (drinks) {
+          const newCocktails = drinks.map((item) => {
+            const {
+              idDrink,
+              strDrink,
+              strDrinkThumb,
+              strAlcoholic,
+              strGlass,
+            } = item;
+            return {
+              id: idDrink,
+              name: strDrink,
+              image: strDrinkThumb,
+              info: strAlcoholic,
+              glass: strGlass,
+            };
+          });
+          setCocktails(newCocktails);
+        } else {
+          setCocktails([]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getDrinks();
+  }, [searchTerm]);
+  return (
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm} />
+      <CocktailsList loading={loading} cocktails={cocktails} />
+    </main>
+  );
 }
