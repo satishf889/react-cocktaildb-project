229fdc99d314a3d71ace0fdded11ce12325e44df
Satish Fulwani <satish.fulwani63@gmail.com>
2020-05-16 18:08:46 +0530

    Add urlParams to get URL parameters

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,5 +1,8 @@
 import React from "react";
+//useParams is the Hook to get url parameters
+import { useParams } from "react-router-dom";
 
 export default function SingleCocktail() {
-  return <h1>single cocktail page</h1>;
+  const { id } = useParams();
+  return <h1>single cocktail page id : {id}</h1>;
 }
